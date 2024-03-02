62079c498de48b34146949ed40dc0bcb2e50c786
KreskaDev <kreskadev@gmail.com>
2024-03-02 23:37:30 +0100

    Add functional tests.

---
+++ Tests/Wth/Wth.Tests/IntegrationTests.cs
@@ -0,0 +1,41 @@
+namespace Wth.Tests
+{
+    public class IntegrationTests
+    {
+        [Fact]
+        public void NewSeller()
+        {
+
+        }
+
+        [Fact]
+        public void ApproveNewSeller()
+        {
+
+        }
+
+        [Fact]
+        public void DeclineNewSeller()
+        {
+
+        }
+
+        [Fact]
+        public void CreateSellerOffer()
+        {
+
+        }
+
+        [Fact]
+        public void DeclineSellerOffer()
+        {
+
+        }
+
+        [Fact]
+        public void RequestBuyOffer()
+        {
+
+        }
+    }
+}

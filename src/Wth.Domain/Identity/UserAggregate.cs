4dc5f5b8f65c91ed5ca929140c2603ab8cb738f1
KreskaDev <kreskadev@gmail.com>
2024-03-12 23:44:02 +0100

    Add user aggregate

---
+++ src/Wth.Domain/Identity/UserAggregate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventFlow.Aggregates;
+using EventFlow.Core;
+
+namespace Wth.Domain.Identity
+{
+    public class UserId :
+        Identity<UserId>
+    {
+        public UserId(string value) : base(value) { }
+    }
+
+    public class UserAggregate :
+        AggregateRoot<UserAggregate, UserId>
+    {
+        public UserAggregate(UserId id) : base(id)
+        {
+        }
+    }
+}
